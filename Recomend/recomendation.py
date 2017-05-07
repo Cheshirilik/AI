83f2bb8ef9a92a1dd3b131fc6c4c4a5403d06873
Cheshirilik <avfriend@mail.ru>
2017-05-07 22:51:23 +0300

    Исправлены опечатки в комментах и названиях ф-ций

--- Recomend/recomendation.py
+++ Recomend/recomendation.py
@@ -38,12 +38,12 @@ def get_euc_distance(obj, name1, name2):
     for i in mas:
         summa += pow(obj[name1][i] - obj[name2][i], 2)
 
-    #return 1 / (1 + summa)
+    # return 1 / (1 + summa)
     return 1/(1 + sqrt(summa))
 
 
 # Подобие корреляцией Пирсона
-def get_pirs_distance(obj, name1, name2):
+def get_pears_distance(obj, name1, name2):
     mas = get_pairs(obj, name1, name2)
 
     n = len(mas)
@@ -56,9 +56,9 @@ def get_pirs_distance(obj, name1, name2):
     sum1_sq = sum([obj[name1][i]**2 for i in mas])
     sum2_sq = sum([obj[name2][i]**2 for i in mas])
 
-    pSum = sum([(obj[name1][i] * obj[name2][i]) for i in mas])
+    p_sum = sum([obj[name1][i] * obj[name2][i] for i in mas])
 
-    num = pSum - sum1 * sum2 / n
+    num = p_sum - sum1 * sum2 / n
     den = sqrt((sum1_sq - sum1**2/n) * (sum2_sq - sum2**2/n))
 
     if den == 0:
@@ -75,23 +75,23 @@ def get_all(obj):
     res1 = []
     for j in temp:
         res.append([j[0] + '-' + j[1], get_euc_distance(obj, j[0], j[1])])
-        res1.append([j[0] + '-' + j[1], get_pirs_distance(obj, j[0], j[1])])
+        res1.append([j[0] + '-' + j[1], get_pears_distance(obj, j[0], j[1])])
 
     return res, res1                # Евклид, Пирсон
 
 
 res_e, res_p = get_all(critics)
-res_e = sorted(res_e, key=lambda x: x[1])                      # сортируем по третьему столбцу (подобию)
+res_e = sorted(res_e, key=lambda x: x[1])                                   # сортируем по третьему столбцу (подобию)
 res_p = sorted(res_p, key=lambda x: x[1])
 
 for i in range(0, len(res_e)):
-    #p = res_p.index(res_e[i][0])
-    print("{0}, euclid = {1}:".format(res_e[i][0], res_e[i][1])) # печатаем все пары
+    print("{0}, euclid = {1}:".format(res_e[i][0], res_e[i][1]))            # печатаем все пары евклида
 
+print("\n")
 for i in range(0, len(res_p)):
-    print("{0}, pirson = {1}:".format(res_p[i][0], res_p[i][1]))  # печатаем все пары
+    print("{0}, pirson = {1}:".format(res_p[i][0], res_p[i][1]))            # печатаем все пары Пирсона
 
-l = len(res_e) - 1                                                        # находим наиболее и наименее похожие пары
+l = len(res_e) - 1                                                          # находим наиболее и наименее похожие пары
 print("\n\nLess similar euclid pair: {0}, distance is {1}: \
       \nMore similar uclid pair: {2}, distance is {3}: \
       ".format(res_e[0][0], res_e[0][1], res_e[l][0], res_e[l][1]))
