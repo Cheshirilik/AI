16f1282ede4fdf775058517aa64ec0f04e765566
Cheshirilik <avfriend@mail.ru>
2017-05-17 21:43:32 +0300

    Добавлена кластеризация методом К-средних

--- Clusters/clusters.py
+++ Clusters/clusters.py
@@ -2,7 +2,9 @@
 
 from math import sqrt
 from pprint import pprint
-from PIL import Image,ImageDraw
+from PIL import Image, ImageDraw
+import random
+import copy
 
 
 # Загрузка данных из файла
@@ -108,7 +110,7 @@ def print_cluster(clust, labels=None, n=0):
 
 # Вычисляем высоту сластеров для корректного отображения на графике
 def get_height(clust):
-    if (clust.left == None) and (clust.right == None):
+    if (clust.left is None) and (clust.right is None):
         return 1
 
     return get_height(clust.left) + get_height(clust.right)
@@ -135,7 +137,7 @@ def draw_node(draw, clust, x, y, scal, labels):
 
         draw.line((x, top + h1/2, x, bottom - h2/2), fill=(255, 0, 0))
         draw.line((x, top + h1/2, x + l, top + h1/2), fill=(255, 0, 0))
-        draw.line((x, bottom - h2/2, x, bottom - h2/2), fill=(255, 0, 0))
+        draw.line((x, bottom - h2/2, x + l, bottom - h2/2), fill=(255, 0, 0))
 
         draw_node(draw, clust.left, x + l, top + h1/2, scal, labels)
         draw_node(draw, clust.right, x + l, bottom - h2/2, scal, labels)
@@ -147,7 +149,7 @@ def draw_node(draw, clust, x, y, scal, labels):
 def draw_diag(clust, labels, jpg="cluster.jpg"):
     h = get_height(clust) * 20
     d = get_depth(clust)
-    w = 1200
+    w = 2000
 
     scal = float(w - 150) / d
 
@@ -159,8 +161,72 @@ def draw_diag(clust, labels, jpg="cluster.jpg"):
     img.save(jpg, "JPEG")
 
 
+# Меняем местами столбцы и строки
+def rotate(clust):
+    new = []
+    for i in range(len(clust)):
+        new_r = [clust[j][i] for j in range(len(data))]
+        new.append(new_r)
+
+    return new
+
+
+# Метод К-средних
+def k_cluster(rows, dist=pearson, n=4):
+
+    # Ищем минимум и максимум для каждого столбца (для генерации случайных центроидов)
+    ranges = [(min([row[i] for row in rows]), max([row[i] for row in rows])) for i in range(len(rows[0]))]
+
+    # Генерируем случайные центроиды (n штук)
+    clusters = [[random.random() * (ranges[i][1] - ranges[i][0]) + ranges[i][0]
+                 for i in range(len(rows[0]))] for j in range(n)]
+
+    last_matches = None
+    for k in range(100):
+        print("Итерация {0}".format(k))
+        best_matches = [[] for i in range(n)]
+
+        for j in range(len(rows)):
+            row = rows[j]
+            best = 0
+            for i in range(n):
+                d = dist(clusters[i], row)
+                if d < dist(clusters[best], row):
+                    best = i
+
+            best_matches[best].append(j)
+
+            if best_matches == last_matches:
+                print(best_matches)
+                print("11111")
+                break
+
+
+            print("777777777777777777777777777777777777777777777777777777777777777")
+            #last_matches = copy.deepcopy(best_matches)
+            last_matches = best_matches
+
+            for i in range(n):
+                avgs = [0.0] * len(rows[0])
+                if len(best_matches[i]) > 0:
+                    for rowid in best_matches[i]:
+                        for m in range(len(rows[rowid])):
+                            avgs[m] += rows[rowid][m]
+                    for j in range(len(avgs)):
+                        avgs[j] /= len(best_matches[i])
+                    clusters[i] = avgs
+
+    return best_matches
+
 # ================== MAIN ==================
 rows, cols, data = read_words('blogdata.txt')
-res = build_clusters(data)
-print_cluster(res, labels=rows)
-draw_diag(res, rows)
+# res = build_clusters(data)
+# # print_cluster(res, labels=rows)
+# draw_diag(res, rows)
+#
+# new = rotate(data)
+# res = build_clusters(data)
+# draw_diag(res, cols, "rotate.jpg")
+
+res = k_cluster(data, n=10)
+print(res)
